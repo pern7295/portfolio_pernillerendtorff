5da38e458a09ec354a6a65eb160881d6a9af1801
pern7295 <pern7295@stud.kea.dk>
2020-12-27 21:24:27 +0100

    NAVBAR VIRKER

--- script.js
+++ script.js
@@ -12,4 +12,10 @@ function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu_img").classList.toggle("change_img");
+
+    document.getElementById("burger1").classList.toggle("change1");
+    document.getElementById("burger2").classList.toggle("change1");
+    document.getElementById("burger3").classList.toggle("change1");
+    document.getElementById("burger4").classList.toggle("change1");
 }
+
