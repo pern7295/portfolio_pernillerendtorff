0209c22ce82c4de03eb2f89fc77308a631a3aaaf
pern7295 <pern7295@stud.kea.dk>
2020-12-27 17:28:30 +0100

    DESKTOP & BURGER

---
+++ script.js
@@ -0,0 +1,15 @@
+/*---BURGERMENU----*/
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menu").addEventListener("click", onClickMenu);
+}
+
+/*--FUNCTION TIL BURGERMENU---*/
+function onClickMenu() {
+    console.log("onClickMenu");
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu_img").classList.toggle("change_img");
+}
