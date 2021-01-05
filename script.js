d290e893fe3643895871de59ca3dcdde0aa58f9b
pern7295 <pern7295@stud.kea.dk>
2021-01-06 00:41:31 +0100

    ready for pics and text

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#menu").addEventListener("click", onClickMenu);
+    document.querySelector("#splash_intro").classList.add("absolute");
+    document.querySelector("#splash_intro").classList.add("splash_ani");
+    document.querySelector("#splash_intro").addEventListener("animationend", splashEnd);
 
 }
 
@@ -18,6 +21,12 @@ function onClickMenu() {
     document.getElementById("burger2").classList.toggle("change1");
     document.getElementById("burger3").classList.toggle("change1");
     document.getElementById("burger4").classList.toggle("change1");
+}
+
+function splashEnd() {
+    console.log ("splashEnd");
+    document.querySelector("#splash_intro").classList.remove("absolute");
+    document.querySelector("#splash_intro").classList.add("sticky");
 }
 
 
